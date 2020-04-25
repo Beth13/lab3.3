9594212e7e9c9819f5d97ac29a29fb5f5c3d43b3
JeePik <andrewlegeza@gmail.com>
2020-04-25 13:01:20 +0300

    Lab3.3

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace lab3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] file = File.ReadAllLines(@"C:\Users\Lenovo X1 Carbon\source\repos\lab3.3\111.txt");
+            int sum; 
+            string[] numbers;
+
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Lenovo X1 Carbon\source\repos\lab3.3\111.txt"))
+            {
+                for (int i = 0; i < file.Length; i++)
+                {
+                    sum = 0;
+                    numbers = file[i].Split(',');
+                    for (int j = 0; j < numbers.Length; j++)
+                        sum += Int32.Parse(numbers[j]);
+                    sw.WriteLine(file[i] + " = " + sum);
+
+                }
+                sw.Close();
+            }
+
+        }
+       
+    }
+   
+}
+
+
